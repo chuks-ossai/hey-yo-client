3ff0211ce85f1e526f6fd07e68b83436246bdcfe
Chuks Ossai <ossaiwisdom48@gmail.com>
2020-09-24 01:34:40 +0100

    Auto navigate user to streams page when he is logged in and closes and reopens his browser

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { TokenStoreService } from './core/services';
 
 @Component({
   selector: 'heyyo-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'client';
+export class AppComponent implements OnInit {
+
+  constructor(private router: Router, private tsService: TokenStoreService) { }
+
+  ngOnInit(): void {
+    this.autoNavigateLoggedinUser();
+  }
+
+  autoNavigateLoggedinUser(): void {
+    if (this.tsService.getToken()) {
+      this.router.navigate(['streams']);
+    }
+  }
 }
